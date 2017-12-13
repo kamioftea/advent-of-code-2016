670252b0cdae803a0a7678baf367885e716e414d
jeff <jeff@goblinoid.co.uk>
2017-12-13 22:59:04 +0000

    Tweaks from writeup of day 1

--- src/test/scala/Day1Test.scala
+++ src/test/scala/Day1Test.scala
@@ -3,20 +3,20 @@ import org.scalatest.{FunSuite, Matchers}
 
 class Day1Test extends FunSuite with Matchers {
 
-  test("can Sum Duplicates") {
-    sumDuplicates("1122") shouldBe 3
-    sumDuplicates("1111") shouldBe 4
-    sumDuplicates("1234") shouldBe 0
-    sumDuplicates("91212129") shouldBe 9
-    sumDuplicates("91222129") shouldBe 13
+    test("can Sum Duplicates") {
+    sumDuplicates("1122".map(_.asDigit)) shouldBe 3
+    sumDuplicates("1111".map(_.asDigit)) shouldBe 4
+    sumDuplicates("1234".map(_.asDigit)) shouldBe 0
+    sumDuplicates("91212129".map(_.asDigit)) shouldBe 9
+    sumDuplicates("91222129".map(_.asDigit)) shouldBe 13
   }
 
 
   test("can Sum Mirrors") {
-    sumMirrors("1212") shouldBe 6
-    sumMirrors("1221") shouldBe 0
-    sumMirrors("123425") shouldBe 4
-    sumMirrors("123123") shouldBe 12
-    sumMirrors("12131415") shouldBe 4
+    sumMirrors("1212".map(_.asDigit)) shouldBe 6
+    sumMirrors("1221".map(_.asDigit)) shouldBe 0
+    sumMirrors("123425".map(_.asDigit)) shouldBe 4
+    sumMirrors("123123".map(_.asDigit)) shouldBe 12
+    sumMirrors("12131415".map(_.asDigit)) shouldBe 4
   }
 }
