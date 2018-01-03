0a175dbb29b6f47a6702cbf483ae072805791754
Jeff Horton <jeff@goblinoid.co.uk>
2018-01-03 23:50:48 +0000

    Tweaks from write up day 23

--- src/main/scala/Day23.scala
+++ src/main/scala/Day23.scala
@@ -4,16 +4,16 @@ import scala.language.implicitConversions
 object Day23 {
 
   sealed trait Value {
-    def v(implicit registers: Map[Char, Long]): Long
+    def read(implicit registers: Map[Char, Long]): Long
   }
 
   case class Ref(register: Char) extends Value {
-    override def v(implicit registers: Map[Char, Long]): Long =
+    override def read(implicit registers: Map[Char, Long]): Long =
       registers.getOrElse(register, 0)
   }
 
   case class Literal(value: Long) extends Value {
-    override def v(implicit registers: Map[Char, Long]): Long = value
+    override def read(implicit registers: Map[Char, Long]): Long = value
   }
 
   case class Instruction(command: String, value1: Value, value2: Value)
@@ -32,16 +32,16 @@ object Day23 {
     def iter(registers: Map[Char, Long], position: Int, mulCount: Int): Int = {
       if (!program.isDefinedAt(position)) mulCount
       else {
-        implicit val _ = registers;
+        implicit val _ = registers
         program(position) match {
           case Instruction("set", Ref(r), v2) =>
-            iter(registers.updated(r, v2.v), position + 1, mulCount)
+            iter(registers.updated(r, v2.read), position + 1, mulCount)
           case Instruction("sub", Ref(r), v2) =>
-            iter(registers.updated(r, registers(r) - v2.v), position + 1, mulCount)
+            iter(registers.updated(r, registers(r) - v2.read), position + 1, mulCount)
           case Instruction("mul", Ref(r), v2) =>
-            iter(registers.updated(r, registers(r) * v2.v), position + 1, mulCount + 1)
+            iter(registers.updated(r, registers(r) * v2.read), position + 1, mulCount + 1)
           case Instruction("jnz", v1, v2) =>
-            iter(registers, if (v1.v != 0) position + v2.v.toInt else position + 1, mulCount)
+            iter(registers, if (v1.read != 0) position + v2.read.toInt else position + 1, mulCount)
         }
       }
     }
@@ -54,11 +54,21 @@ object Day23 {
     val start = seed * 100 + 100000
     val end = start + 17000
 
-    def iter(b: Int, d: Int = 2, h: Int = 0): Int = {
+    def hasFactor(b: Int): Boolean = {
+      val rootB = Math.sqrt(b).toInt
+
+      def iter(d: Int): Boolean = {
+        if(d > rootB) return false
+        if(b % d == 0) return true
+        iter(d + 1)
+      }
+
+      iter(2)
+    }
+
+    def iter(b: Int, h: Int = 0): Int = {
       if(b > end) return h
-      if(d == b) return iter(b + 17, 2, h)
-      if(b % d == 0) return iter(b + 17, 2, h + 1)
-      iter(b, d + 1, h)
+      iter(b + 17, if(hasFactor(b)) h + 1 else h)
     }
 
     iter(start)
